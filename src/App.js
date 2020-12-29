a5b60dcfba32999d2f910d8cbd6123358d687d51
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2020-12-29 14:17:13 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <Container className='mt-5'>
+          <div className='header'>
+            <p className=''>Welcome to App Platform</p>
+          </div>
+        </Container>
       </header>
+      <main>
+        <h1 className=''>Guest Book</h1>
+        <p>Body</p>
+      </main>
+      <footer></footer>
     </div>
   );
 }
